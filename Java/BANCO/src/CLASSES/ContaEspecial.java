b1da5dc152d48598179a0434ea448a4d1f974076
Maurício Nobre <mauricio_nobhre@hotmail.com>
2021-03-08 09:39:25 -0300

    Projeto atualizado

--- Java/BANCO/src/CLASSES/ContaEspecial.java
+++ Java/BANCO/src/CLASSES/ContaEspecial.java
@@ -4,59 +4,50 @@ import java.util.Scanner;
 
 public class ContaEspecial extends Conta {
 
-      double LimiteEspecial = 1000;
+    double limiteEspecial = 1000;
     Scanner s = new Scanner(System.in);
+    
     public ContaEspecial() {
-
-    System.out.println("Logue na sua conta.");
-    System.out.println("Digite o numero da sua conta:");
-    numero = s.nextInt();
-    System.out.println("Digite o seu CPF: ");
-    cpf = s.next();
-    System.out.println("Essa conta esta ativa?");
-    System.out.println();
-    System.out.println("[1]Ativa[2]inativa");
-    char c = s.next().charAt(0);
-    if(c == 1) ativa = true;
-    if(c == 2) ativa = false;
-
-
-
-
-     }
+	    System.out.println("Logue na sua conta.");
+	    System.out.println("Digite o numero da sua conta:");
+	    numero = s.nextInt();
+	    System.out.println("Digite o seu CPF: ");
+	    cpf = s.next();
+	    System.out.println("Essa conta esta ativa?");
+	    System.out.println();
+	    System.out.println("[1]Ativa[2]inativa");
+	    char c = s.next().charAt(0);
+	    if(c == 1) ativa = true;
+	    if(c == 2) ativa = false;
+    }
+    
     @Override
     public void Debito(double valor) {
-        if(getSaldo() - valor >=0) {
-
+    	double saldoAtual = getSaldo();  
+    	
+        if(valor <= saldoAtual) {
             super.Debito(valor);
         }
-        else {
-
-            if(getSaldo() + LimiteEspecial > valor) {
-                System.out.println("Saldo insuficiente...");
-                System.out.println("...usando limite");
-                System.out.print(LimiteEspecial);
-
-                UsarLimite(valor);
-                System.out.println("==>"+LimiteEspecial);
-
-                Debito(valor);
-            }
-
-            else {
-                System.out.println(" Saldo e limete insuficiente ");
-            }
-
-
-        }
+        
+        if(valor > saldoAtual) {
+        	double limiteNecessario = valor - saldoAtual; 
+        	if (limiteNecessario > limiteEspecial) {
+        		System.out.println("Saldo e limite insuficientes para realizar o debito...");
+        		return;
+        	}
+        	
+        	if(saldoAtual > 0) {
+        		System.out.println("Debitando "+saldoAtual+" da conta");
+        		super.Debito(saldoAtual);
+        	}
+        	
+        	usarLimite(limiteNecessario);
+        }        
     }
-
-
-
-        public void UsarLimite(double valor) {
-            double restante = valor - getSaldo();
-            super.Credito(restante);
-            LimiteEspecial = LimiteEspecial - restante;
-
-        }
+    
+    public void usarLimite(double valor) {
+    	System.out.println("Utilizando R$"+valor+" do limite especial...");
+        limiteEspecial = limiteEspecial - valor;
+        System.out.println("Limite especial restante R$"+limiteEspecial + " utilize com sabedoria :)");
+    }
 }
